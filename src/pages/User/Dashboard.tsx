e5370e535097f929f59ab37ceb89b6dabc12e633
Vladimir Sulovic <sulovic@gmail.com>
2025-12-22 22:57:30 +0100

    Stareted work on users in separate branch

--- src/pages/User/Dashboard.tsx
+++ src/pages/User/Dashboard.tsx
@@ -1,34 +1,48 @@
 import { useEffect, useState } from "react";
-import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 import Modal from "../../components/Modal";
 import ModalEditUser from "./ModalEditUser";
 import { toast } from "react-toastify";
 import Spinner from "../../components/Spinner";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
+import Filters from "../../components/Filters";
+import Search from "../../components/Search";
+import Pagination from "../../components/Pagination";
+import dataServiceBuilder from "../../services/dataService";
+import { handleCustomErrors } from "../../services/errorHandler";
 
 const Dashboard: React.FC = () => {
-  const [tableData, setTableData] = useState<User[]>([]);
+  const [tableData, setTableData] = useState<UserData[]>([]);
   const [showSpinner, setShowSpinner] = useState(false);
   const [showModal, setShowModal] = useState(false);
   const [showModalEditUser, setShowModalEditUser] = useState(false);
   const [selectedUserEmail, setSelectedUserEmail] = useState("");
-  const [updateData, setUpdateData] = useState<User | null>(null);
+  const [updateData, setUpdateData] = useState<UserData | null>(null);
   const navigate = useNavigate();
   const axiosPrivate = useAxiosPrivate();
   const { authUser } = useAuth();
   const tableHeaders = ["Ime i prezime", "Email", "Nivo ovlašćenja"];
+  const usersService = dataServiceBuilder<UserData>(axiosPrivate, authUser, "users");
+  const [queryParams, setQueryParams] = useState<QueryParams>({ filters: { roleId: "*" }, page: 1, limit: 20, sortOrder: "desc", sortBy: "userId" });
+  const filtersOptions: FiltersOptions = {
+    roleId: ["1001", "3001", "5001"],
+  };
+
+
 
   const fetchData = async () => {
     setShowSpinner(true);
 
     try {
-      const response = await axiosPrivate.get("users");
-      setTableData(response?.data || []);
+      const [response, usersCount] = await Promise.all([
+        usersService.getAllResources(queryParams),
+        usersService.getAllResourcesCount(queryParams),
+      ]);
+      setTableData(response.data.data);
+      setQueryParams({ ...queryParams, count: usersCount.data.count });
     } catch (error) {
-      toast.error(`UPS!!! Došlo je do greške: ${error} `, {
-        position: "top-center",
-      });
+      handleCustomErrors(error as string);
     } finally {
       setShowSpinner(false);
     }
@@ -37,7 +51,7 @@ const Dashboard: React.FC = () => {
   useEffect(() => {
     fetchData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [queryParams.filters, queryParams.search, queryParams.page, queryParams.limit, queryParams.sortOrder, queryParams.sortBy]);
 
   const handleDeleteOK = async () => {
     setShowSpinner(true);
@@ -83,7 +97,10 @@ const Dashboard: React.FC = () => {
               Dodaj korisnika
             </button>
           </div>
-
+          <div className="mb-4 flex gap-4 justify-end">
+            <Filters filtersOptions={filtersOptions} queryParams={queryParams} setQueryParams={setQueryParams} />
+            <Search queryParams={queryParams} setQueryParams={setQueryParams} />
+          </div>
           <div>
             <div className="relative my-4 overflow-x-auto shadow-lg sm:rounded-lg">
               <div className="table-responsive p-3">
@@ -106,10 +123,10 @@ const Dashboard: React.FC = () => {
                   <tbody>
                     {tableData.map((row, index) => (
                       <tr key={index} className="border-b bg-white hover:!bg-zinc-100 dark:border-zinc-700 dark:bg-zinc-800">
-                        <td key={`ime_prezime_${index}`}>{row?.ime_prezime}</td>
-                        <td key={`email_${index}`}>{row?.email}</td>
-                        <td key={`role_${index}`}>{row?.role?.role}</td>
-                        <td key={`editUser_${index}`} className="text-center">
+                        <td>{row.firstName} {row?.lastName}</td>
+                        <td >{row.email}</td>
+                        <td>{row.roleName}</td>
+                        <td className="text-center">
                           <button type="button" className="button button-sky" aria-label="EditUser" disabled={authUser?.email === row?.email} onClick={() => handleEdit(row)}>
                             Izmeni
                           </button>
@@ -129,6 +146,9 @@ const Dashboard: React.FC = () => {
                 {updateData && showModalEditUser && <ModalEditUser setShowModalEditUser={setShowModalEditUser} updateData={updateData} setUpdateData={setUpdateData} fetchData={fetchData} />}
               </div>
             </div>
+          </div>
+          <div className="flex justify-end gap-4 mb-4">
+            <Pagination queryParams={queryParams} setQueryParams={setQueryParams} />
           </div>
         </>
       ) : (
