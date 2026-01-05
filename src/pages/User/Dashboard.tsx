47025c4e9b90d70df77d5378c324cfe2740b9af1
Vladimir Sulovic <sulovic@gmail.com>
2026-01-05 08:48:35 +0100

    Started work on users Dashboard

--- src/pages/User/Dashboard.tsx
+++ src/pages/User/Dashboard.tsx
@@ -1,16 +1,16 @@
 import { useEffect, useState } from "react";
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 import Modal from "../../components/Modal";
 import ModalEditUser from "./ModalEditUser";
 import { toast } from "react-toastify";
 import Spinner from "../../components/Spinner";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
-import useAxiosPrivate from "../../hooks/useAxiosPrivate";
-import Filters from "../../components/Filters";
-import Search from "../../components/Search";
-import Pagination from "../../components/Pagination";
 import dataServiceBuilder from "../../services/dataService";
 import { handleCustomErrors } from "../../services/errorHandler";
+import Search from "../../components/Search";
+import Pagination from "../../components/Pagination";
+import Filters from "../../components/Filters";
 
 const Dashboard: React.FC = () => {
   const [tableData, setTableData] = useState<UserData[]>([]);
@@ -23,24 +23,15 @@ const Dashboard: React.FC = () => {
   const axiosPrivate = useAxiosPrivate();
   const { authUser } = useAuth();
   const tableHeaders = ["Ime i prezime", "Email", "Nivo ovlašćenja"];
-  const usersService = dataServiceBuilder<UserData>(axiosPrivate, authUser, "users");
-  const [queryParams, setQueryParams] = useState<QueryParams>({ filters: { roleId: "*" }, page: 1, limit: 20, sortOrder: "desc", sortBy: "userId" });
-  const filtersOptions: FiltersOptions = {
-    roleId: ["1001", "3001", "5001"],
-  };
-
-
+  const userService = dataServiceBuilder<UserData>(axiosPrivate, authUser, "users");
+  const [queryParams, setQueryParams] = useState<QueryParams>({ filters: {}, page: 1, limit: 20, sortOrder: "desc", sortBy: "userId" });
 
   const fetchData = async () => {
     setShowSpinner(true);
 
     try {
-      const [response, usersCount] = await Promise.all([
-        usersService.getAllResources(queryParams),
-        usersService.getAllResourcesCount(queryParams),
-      ]);
-      setTableData(response.data.data);
-      setQueryParams({ ...queryParams, count: usersCount.data.count });
+      const response = await userService.getAllResources(queryParams);
+      setTableData(response.data.data || []);
     } catch (error) {
       handleCustomErrors(error as string);
     } finally {
@@ -51,7 +42,7 @@ const Dashboard: React.FC = () => {
   useEffect(() => {
     fetchData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [queryParams.filters, queryParams.search, queryParams.page, queryParams.limit, queryParams.sortOrder, queryParams.sortBy]);
+  }, []);
 
   const handleDeleteOK = async () => {
     setShowSpinner(true);
@@ -81,7 +72,7 @@ const Dashboard: React.FC = () => {
     setShowSpinner(false);
   };
 
-  const handleEdit = (row: any) => {
+  const handleEdit = (row: UserData) => {
     setUpdateData(row);
     setShowModalEditUser(true);
   };
@@ -97,10 +88,7 @@ const Dashboard: React.FC = () => {
               Dodaj korisnika
             </button>
           </div>
-          <div className="mb-4 flex gap-4 justify-end">
-            <Filters filtersOptions={filtersOptions} queryParams={queryParams} setQueryParams={setQueryParams} />
-            <Search queryParams={queryParams} setQueryParams={setQueryParams} />
-          </div>
+
           <div>
             <div className="relative my-4 overflow-x-auto shadow-lg sm:rounded-lg">
               <div className="table-responsive p-3">
@@ -122,17 +110,19 @@ const Dashboard: React.FC = () => {
                   </thead>
                   <tbody>
                     {tableData.map((row, index) => (
-                      <tr key={index} className="border-b bg-white hover:!bg-zinc-100 dark:border-zinc-700 dark:bg-zinc-800">
-                        <td>{row.firstName} {row?.lastName}</td>
-                        <td >{row.email}</td>
-                        <td>{row.roleName}</td>
-                        <td className="text-center">
+                      <tr key={index} className="border-b bg-white hover:bg-zinc-100! dark:border-zinc-700 dark:bg-zinc-800">
+                        <td key={`ime_prezime_${index}`}>
+                          {row.firstName} {row.lastName}
+                        </td>
+                        <td key={`email_${index}`}>{row.email}</td>
+                        <td key={`role_${index}`}>{row.roleName}</td>
+                        <td key={`editUser_${index}`} className="text-center">
                           <button type="button" className="button button-sky" aria-label="EditUser" disabled={authUser?.email === row?.email} onClick={() => handleEdit(row)}>
                             Izmeni
                           </button>
                         </td>
                         <td key={`deleteUser_${index}`} className="text-center">
-                          <button type="button" className="button button-red" aria-label="Delete" disabled={authUser?.email === row?.email || !authUser?.superAdmin} onClick={() => handleDelete(row?.email)}>
+                          <button type="button" className="button button-red" aria-label="Delete" disabled={authUser?.email === row?.email || !authUser?.superAdmin} onClick={() => handleDelete(row.email)}>
                             Obriši
                           </button>
                         </td>
@@ -146,9 +136,6 @@ const Dashboard: React.FC = () => {
                 {updateData && showModalEditUser && <ModalEditUser setShowModalEditUser={setShowModalEditUser} updateData={updateData} setUpdateData={setUpdateData} fetchData={fetchData} />}
               </div>
             </div>
-          </div>
-          <div className="flex justify-end gap-4 mb-4">
-            <Pagination queryParams={queryParams} setQueryParams={setQueryParams} />
           </div>
         </>
       ) : (
